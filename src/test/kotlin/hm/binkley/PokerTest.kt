8c188f04b84231a614f94e1fd74e70f4fd55be8d
Brian Oxley <boxley@thoughtworks.com>
2023-01-11 08:15:55 -0600

    More idiomatic

--- src/test/kotlin/hm/binkley/PokerTest.kt
+++ src/test/kotlin/hm/binkley/PokerTest.kt
@@ -13,7 +13,7 @@ import hm.binkley.Suit.SPADES
 import io.kotest.matchers.comparables.shouldBeGreaterThan
 import org.junit.Test
 
-internal class PokerTest {
+class PokerTest {
     @Test
     fun `should rank one of a kind`() {
         val handA = Hand(
